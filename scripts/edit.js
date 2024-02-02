fa5e0366500f3a0992126205ec6a31b3c89ccbb3
Palili <parisasafipour1999@gmail.com>
2024-02-02 23:20:37 +0330

    feature: add edit page

---
+++ scripts/edit.js
@@ -0,0 +1,24 @@
+const menubtn = document.getElementById('menu-button');
+const sidebar = document.getElementById('sidebar-container');
+
+const container = document.getElementById('container');
+
+menubtn.onclick = () => {
+  sidebar.style.width = (sidebar.style.width === '15%') ? '10%' : '15%';
+};
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  if (form.checkValidity()) {
+    document.location.replace('./articles.html?edit=success');
+  }
+  form.classList.add('was-validated');
+});
+
+
+document.getElementById('Logout').addEventListener('click', function () {
+  location.href = './login.html';
+});
+ 
